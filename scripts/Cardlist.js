965f5983c92407b09f427580b60563630d0bcf9b
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-06-06 15:48:41 +0300

    sprint 9 with comments to fix sprint 8

--- scripts/Cardlist.js
+++ scripts/Cardlist.js
@@ -12,6 +12,14 @@ class Cardlist {
 
     render() {
         this._startCards.forEach((card) => {
+            /* Можно лучше:
+
+            Избавиться от дублирования кода, и вместо того, чтобы переписывать реализацию метода addCard повторяя один и тот же 
+            код два раза, можно напрямую вызвать метод addCard в методе render и передать card.name и card.link в качестве параметров
+
+            Например:
+            this.addCard(card.name, card.link)
+            */
             this._container.appendChild(this._createCard(card.name, card.link, this._openPopupMethod).create());
         });
     }
--- scripts/Popup.js
+++ scripts/Popup.js
@@ -11,6 +11,12 @@ class Popup {
        this._toggle();
     }
 
+    /* Можно лучше:
+
+	Реализовать закрытие попапа по клику на Escape(keycode = 27), либо по клику вне попапа
+
+	Подробнее можно узнать здесь: https://stackoverflow.com/questions/1481626/how-to-handle-esc-keydown-on-javascript-popup-window
+	*/
     close() {
         this._toggle();
     }
